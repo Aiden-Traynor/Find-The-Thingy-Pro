ad2b4c02da83106f61b8953b3c63129e1bd87b5b
Oliver123epic <72059535+Oliver123epic@users.noreply.github.com>
2021-01-21 10:52:47 -0800

    Newest version of find the thingy

--- Assets/FindTheThingyMoveScript.cs
+++ Assets/FindTheThingyMoveScript.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,10 +8,18 @@ public class FindTheThingyMoveScript : MonoBehaviour
   public float RotationSpeed;
   Rigidbody R;
   public float JumpHeight;
+  bool jumping = false;
+  bool OnCollisionEnter;
     // Start is called before the first frame update
     void Start()
     {
-
+void OnCollisionEnter(Collider other) 
+{
+		if(other.tag.Equals("Ground")) //if we hit the floor, we are no longer jumping
+{
+	jumping = false;
+		}
+	}
       Speed = 8f;
       RotationSpeed = 130f;
       R = GetComponent<Rigidbody>();
@@ -22,6 +30,8 @@ public class FindTheThingyMoveScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+
       if (Input.GetKey(KeyCode.UpArrow)){
           //move forward line
           R.MovePosition(transform.position + transform.forward * Speed * Time.deltaTime);
@@ -38,10 +48,10 @@ public class FindTheThingyMoveScript : MonoBehaviour
           //rotate right line
           transform.Rotate(Vector3.up * RotationSpeed * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.Space)){
+        if (!jumping && Input.GetKey(KeyCode.Space)){
             //move forward line
             R.MovePosition(transform.position + transform.up * JumpHeight );
-
+	jumping = true;
 
             // if (Input.GetKey(KeyCode.UpArrow)){
             //     //move forward line
