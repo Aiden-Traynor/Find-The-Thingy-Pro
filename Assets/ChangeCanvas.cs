efd04647aac3c45189a05597a557e5278f5e7fa3
Aiden-Traynor <atraynor@springstreet.org>
2021-05-02 12:36:39 -0700

    Got main menu to work, added music and lastly, no

---
+++ Assets/ChangeCanvas.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCanvas : MonoBehaviour
+{
+  public GameObject HTP;
+  public GameObject MM;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void SwitchCanvas() {
+	  HTP.SetActive(false);
+	  MM.SetActive(true);
+}
+
+}
---
+++ Assets/PlayScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayScript : MonoBehaviour
+{
+  public GameObject menuCamera; /* this will be the Camera object that looks at the main menu */
+  public GameObject playerCamera; /* this will be the Camera object that looks at the player */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void BeginGame()
+    {
+      //GameObject.FindGameObjectWithTag("Main Menu Music").GetComponent<AudioSource>().Stop();
+      GameObject.FindGameObjectWithTag("FTTP Music").GetComponent<AudioSource>().Play();
+
+	  menuCamera.SetActive(false);
+	  playerCamera.SetActive(true);
+}
+
+}
