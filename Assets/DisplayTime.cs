baa7efcceb3922f46df345d81fd98fc45086d84a
Oliver123epic <72059535+Oliver123epic@users.noreply.github.com>
2021-05-04 11:15:49 -0700

    I guess you could say I did a die script thing?

--- Assets/DisplayTime.cs
+++ Assets/DisplayTime.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -19,6 +19,6 @@ public class DisplayTime : MonoBehaviour
     void Update()
     {
 
-      t.text = "Time: " + (Time.timeSinceLevelLoad - tig.TimeInMenu);
+      t.text = "Time: " + (Time.timeSinceLevelLoad + tig.TotalTime - tig.TimeInMenu);
     }
 }
